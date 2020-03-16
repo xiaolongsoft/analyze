c8aa156d5aa77d7b6a83eb043eb9ed6a5582d13f
晓龙 <oncwnuFwKMVesFRFJmcVbgv2PR1c@git.weixin.qq.com>
2020-03-16 17:44:22 +0800

    Initial commit

---
+++ src/test/java/ftjw/web/mobile/analyze/AnalyzeApplicationTests.java
@@ -0,0 +1,13 @@
+package ftjw.web.mobile.analyze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
